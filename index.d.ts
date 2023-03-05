cd1197d12de55f0ba99d7b5975b0b7d16a375677
Firzan Armani <mrfirzanarmani@gmail.com>
2023-03-06 01:18:22 +0800

    modify types

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ declare function useImage(
   url: string,
   crossOrigin?: 'anonymous' | 'use-credentials',
   referrerpolicy?: 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'same-origin' | 'strict-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'
-): [undefined | HTMLImageElement, 'loaded' | 'loading' | 'failed'];
+): [undefined , 'loading'] | [undefined, 'failed'] | [HTMLImageElement, 'loaded'];
 
 export default useImage;
